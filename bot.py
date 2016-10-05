4b3c844f0ded1e854a99de4014b5c8b3c691a3cf
rastice <rastice@yandex.ru>
2016-10-05 18:00:54 +0300

    resend messages from vk to telegram

--- bot.py
+++ bot.py
@@ -1,14 +1,22 @@
 import telebot
 import chat_messages as cm
-import vk_messages_control as vk_mc
+from vk_messages_control import Message, User
 bot = telebot.TeleBot('288577480:AAHLCAz53rhF0T14CL80qJLIiPdbupqbvbc')
 
-session = None
+user = User('harovod@mail.ru','kinoprom12')
+user.auth()
 
 @bot.message_handler(commands=['start'])
 def hello_message(message):
 	bot.send_message(message.chat.id, cm.hello_msg(message.chat.first_name, message.chat.last_name))
-	session = vk_mc.User(input(),input()).auth()
+
+
+@bot.message_handler(commands=['get'])
+def get_message_from_vk(message):
+	messages = user.get_messages()
+	print(messages)
+	for msg in messages:
+		bot.send_message(message.chat.id, msg)
 
 
 if __name__ == '__main__':
