ab03a9a052e225268442c600d1cdcb5bb3021367
rastice <rastice@yandex.ru>
2016-10-05 13:05:00 +0300

    Ready to send message obj to Telegram

--- vk_messages_control.py
+++ vk_messages_control.py
@@ -41,17 +41,17 @@ class User:
 
     @safe_request
     def get_messages(self):
-        unread_msgs = []
-        print('1')
-        messages = self.api.messages.get(out=0, count=2)
-        print(messages)
-        for message in messages:
-            if message['read_state'] == 0:
-                user_info = self.api.users.gets(user_ids=message['user_id'])
-                unread_msgs.append(Message(user_info['first_name'],user_info['last_name'],message['body']).get_text())
-            print('2')       
+        unread_msgs = list()
+        messages = self.api.messages.get(out=0, count=500)
+        for i in range(1, len(messages)):
+            if messages[i]['read_state'] == 0:
+                user_info = self.api.users.get(user_id = messages[i]['uid'])
+                print(user_info)
+                msg = Message(user_info[0]['first_name'],user_info[0]['last_name'], messages[i]['body'])
+                unread_msgs.append(msg)                
         return unread_msgs
-        
+
+
 u = User(input(),input())
 u.auth()
 u.get_messages()
